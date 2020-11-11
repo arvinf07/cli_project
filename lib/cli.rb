6da8c2ae2ce7712f22675b2d261bdc3c1a35226d
Arvin Fernandez <arvinf07@gmail.com>
2020-11-11 17:07:25 -0500

    implemented obj reusability

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,6 +4,28 @@ class Cli
   def start
     puts "Welcome"
   end
+
+   # def forecast_display
+  #   if @state == "Clear" && @current_temp > 50 && @@all.first == self
+  #     puts "Todays weather is looking good!\n\n"
+  #     puts "The current temperature is #{@current_temp}°F, with the a max of #{@max_temp}°F and a low of #{@min_temp}°F"
+  #     puts "Wind speed of #{@wind.to_i}mph " 
+  #     puts "A Clear sky with no chance of rain"
+  #   end  
+  # end  
+
+  # def self.today_forecast
+  #   if 
+  #     puts "Todays weather looks good!"
+
+  #   end  
+  # end  
+
+  # def self.forecast
+  #   puts "The weather in #{@@city} for the next 5 days:"
+  #   @@all.each do |weather|
+
+  #   end 
   
   
 end  
--- lib/weather.rb
+++ lib/weather.rb
@@ -3,7 +3,6 @@ require_relative "../environment.rb"
 require_relative "./api.rb"
 
 class Weather
-  @@city = api.city_title
   @@all = []
 
   def initialize(attributes)
@@ -28,28 +27,5 @@ class Weather
     @min_temp = (@min_temp * 1.8 + (32)).to_i
     @max_temp = (@max_temp * 1.8 + (32)).to_i
   end  
-
-  def forecast_display
-    if @state == "Clear" && @current_temp > 50 && @@all.first == self
-      puts "Todays weather is looking good!\n\n"
-      puts "The current temperature is #{@current_temp}°F, with the a max of #{@max_temp}°F and a low of #{@min_temp}°F"
-      puts "Wind speed of #{@wind.to_i}mph " 
-      puts "A Clear sky with 0% chance of rain"
-    end  
-  end  
-
-  def self.today_forecast
-    if 
-      puts "Todays weather looks good!"
-
-    end  
-  end  
-
-  def self.forecast
-    puts "The weather in #{@@city} for the next 5 days:"
-    @@all.each do |weather|
-
-    end 
-    
-  end  
+  
 end  
